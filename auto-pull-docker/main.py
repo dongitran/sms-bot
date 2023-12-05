d014291be38028410a89b6ba5d163212fe73ffec
Dong Tran <dong.tt@urbox.vn>
2023-12-05 18:00:13 +0700

    update script auto pull docker

--- auto-pull-docker/main.py
+++ auto-pull-docker/main.py
@@ -3,6 +3,7 @@ import time
 import logging
 from logging.handlers import RotatingFileHandler
 from telegram import Bot
+import requests
 
 TELEGRAM_BOT_TOKEN = ''
 TELEGRAM_CHAT_ID = ''
@@ -18,36 +19,40 @@ logger = logging.getLogger()
 logger.addHandler(log_handler)
 logger.setLevel(logging.INFO)
 
+def get_image_info(image_name):
+    try:
+        response = requests.get(f"https://hub.docker.com/v2/repositories/{image_name}/tags/latest")
+        if response.status_code == 200:
+            image_info = response.json()
+            return image_info
+        else:
+            print(f"Failed to get image info, status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
+        return None
+
 def check_and_update_image(container_name, image_name):
     client = docker.from_env()
 
     logger.info(f"Start pull image..")
-    try:
-        client.images.pull(image_name)
-    except docker.errors.ImageNotFound:
-        logger.error(f"Image '{image_name}' not exist")
-        return
-    except docker.errors.APIError as e:
-        logger.error(f"APIError: {e}")
-        return
 
     try:
+        version = get_image_info(image_name)
+        digest = image_name + '@' + version['digest']
+        
         container = client.containers.get(container_name)
         current_image_id = container.image.id
         current_image_digest = container.image.attrs['RepoDigests'][0] if 'RepoDigests' in container.image.attrs else None
 
-        latest_image = client.images.get(image_name)
-        latest_image_id = latest_image.id
-        latest_image_digest = latest_image.attrs['RepoDigests'][0] if 'RepoDigests' in latest_image.attrs else None
-
         logger.info(f"current_image_id {current_image_id}")
-        logger.info(f"latest_image_id {latest_image_id}")
         logger.info(f"current_image_digest {current_image_digest}")
-        logger.info(f"latest_image_digest {latest_image_digest}")
+        logger.info(f"digest {digest}")
 
-        if current_image_id != latest_image_id or current_image_digest != latest_image_digest:
+        if current_image_digest != digest:
             logger.info("Updating...")
             send_telegram_message("🚀 <b>Auto Pull Docker - Raspberry pi</b> Deployment has started!")
+            client.images.pull(image_name)
             container.stop()
             container.remove()
 
@@ -60,8 +65,13 @@ def check_and_update_image(container_name, image_name):
             logger.info("No action.")
     except docker.errors.NotFound as e:
         logger.error(f"Container '{container_name}' not found: {e}")
+        return
     except docker.errors.APIError as e:
         logger.error(f"APIError: {e}")
+        return
+    except docker.errors.ImageNotFound:
+        logger.error(f"Image '{image_name}' not exist")
+        return
 
 if __name__ == "__main__":
     container_name = ""
@@ -69,4 +79,4 @@ if __name__ == "__main__":
 
     while True:
         check_and_update_image(container_name, image_name)
-        time.sleep(60)
+        time.sleep(30)
