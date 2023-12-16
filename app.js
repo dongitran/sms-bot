0297a06be8b81649cd2d24a34ca31cf419d75bfa
dongtran <thiendong.iuh@gmail.com>
2023-12-16 16:48:13 +0700

    add test

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const job = schedule.scheduleJob("*/1 * * * * *", async function () {
     if (msgSendTelegram.length > 0) {
       try {
         bot.telegram.sendMessage(
-          process.env.TELEGRAM_GROUP_ID,
+          '-1002144962138',
           msgSendTelegram,
           {
             parse_mode: "HTML",
